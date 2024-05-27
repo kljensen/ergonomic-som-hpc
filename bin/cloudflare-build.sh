bc32422295174cc74ca69469ce21b1c4d3ad0c0c
Kyle L. Jensen <kljensen@gmail.com>
2024-05-27 09:50:48 -0400

    Fix typo

--- bin/cloudflare-build.sh
+++ bin/cloudflare-build.sh
@@ -6,7 +6,9 @@
 
 # Download zola
 ZOLA_VERSION="0.18.0"
-ZOLA_URL="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-curl -sL $ZOLA_URL | tar xz
+ZOLA_URL="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+curl -LO $ZOLA_URL
+tar -xzf zola-${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+
 chmod +x zola
 zola build
