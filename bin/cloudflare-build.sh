97fa3b45bf25205f48fbd6add7fe83754deb0a28
Kyle L. Jensen <kljensen@gmail.com>
2024-05-27 09:53:23 -0400

    Logging

--- bin/cloudflare-build.sh
+++ bin/cloudflare-build.sh
@@ -6,7 +6,12 @@
 
 # Download zola
 ZOLA_VERSION="0.18.0"
+echo "Downloading Zola v${ZOLA_VERSION}"
 ZOLA_URL="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+echo "Downloading Zola from $ZOLA_URL"
 curl -sL $ZOLA_URL | tar xz
+echo "Downloaded Zola"
 chmod +x zola
-zola build
+echo "Zola is executable"
+./zola build
+echo "Zola build complete"
